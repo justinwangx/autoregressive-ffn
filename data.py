3986a7b7961ea26f21a724ec1e5b2b4e1c0159f1
Justin Wang <justinwang46@gmail.com>
2023-02-28 22:54:25 -0500

    better batch creation, extended to n-gram modeling

--- data.py
+++ data.py
@@ -1,9 +1,8 @@
+import numpy as np
 import torch
-import random
-import re
 
 class Data():
-    def __init__(self):
+    def __init__(self, context_length):
         # all characters (0,115,394 total)
         # unique characters (65 total)
         self.data = open('data/tinyshakespeare.txt', 'r').read()
@@ -11,37 +10,25 @@ class Data():
         self.vocab_size = len(self.chars)
         self.cti = {c:i for i,c in enumerate(self.chars)}
         self.itc = {i:c for i,c in enumerate(self.chars)}
+        self.context_length = context_length
 
-        # split data into sentences
-        # self.data = re.split('r[.?! \n]+', self.data)
-        self.data = self.data.splitlines()
-        while '' in self.data:
-            self.data.remove('')
-        # create train and test splits
-        split = int(0.9*len(self.data))
-        self.train_data = self.data[:split]
-        self.test_data = self.data[split:]
+        # convert data to np array of ints
+        data = []
+        for ch in self.data:
+            data.append(self.cti[ch])
+        self.data = np.array(data)
+
+        # create dataset of character ngrams, train and test splits
+        split = int(0.9 * self.data.shape[0])
+        strides = self.data.strides[0]
+        self.data = np.lib.stride_tricks.as_strided(data, shape=(self.data.shape[0]-context_length, context_length+1), strides=(strides, strides))
+        X, Y = self.data[:, :context_length], self.data[:, context_length]
+        self.Xtrain, self.Xtest = X[:split], X[split:]
+        self.Ytrain, self.Ytest = Y[:split], Y[split:]
+        del self.data
     
     def get_batch(self, batch_size, split='train'):
-        data = self.train_data if split=='train' else self.test_data
+        X, Y = (self.Xtrain, self.Ytrain) if split=='train' else (self.Xtest, self.Ytest)
         # next-character pairs
-        x = []
-        y = []
-        # x = torch.tensor(batch_size, 1)
-        # y = torch.tensor(batch_size, 1)
-        for i in range(batch_size):
-            sentence = random.choice(data) + '\n'
-
-            # sentence = data[0] # check to overfit on a sentence
-            while len(sentence) < 2:
-                sentence = random.choice(data)
-            idx = random.randint(0, len(sentence)-2)
-            x.append(self.cti[sentence[idx]])
-            y.append(self.cti[sentence[idx+1]])
-        return torch.tensor(x), torch.tensor(y)
-
-    def encode(self, chars):
-        return [self.cti[c] for c in chars]
-
-    def decode(self, nums):
-        return [self.itc[n] for n in nums]
+        batch_indices = np.random.randint(0, X.shape[0], (batch_size,))
+        return torch.tensor(X[batch_indices]), torch.tensor(Y[batch_indices])
